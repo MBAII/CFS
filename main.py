9a5ace987624b33fa9fcb4baa91636b5c5d4e327
Mingze Bai <baimingze369@gmail.com>
2018-11-07 09:39:56 -0500

    Nov 7th copy

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ vix_global = []
 sixty_day_ave_price = []
 hundrend_twenty_ave_price = []
 
+#below lists are for Var
+path_list = []## random generated paths
+last_day_price = []
 
 ##Assume we are able to short
 def Portfolio1_cumulative():
@@ -81,8 +84,8 @@ def Portfolio2_cumulative():
 
 
 #only hold each stock one day and sell it on the next day
-def Portfolio1():
-    length = len(sp_price_global)
+def Portfolio1(stock_list):
+    length = len(stock_list)
     daily_return_list =[0]
     daily_return_cumulative = [0]
     total_return = 0
@@ -93,7 +96,7 @@ def Portfolio1():
         index_holding = -1
 
     for i in range(1,length):
-        daily_return = (sp_price_global[i] - sp_price_global[i-1]) * index_holding
+        daily_return = (stock_list[i] - stock_list[i-1]) * index_holding
         total_return += daily_return
         daily_return_cumulative.append(total_return)
         daily_return_list.append(daily_return)
@@ -104,8 +107,8 @@ def Portfolio1():
     return total_return, daily_return_list,daily_return_cumulative
 
 
-def Portfolio2():
-    length = len(sp_price_global)
+def Portfolio2(stock_list):
+    length = len(stock_list)
 
     daily_return_list = [0]
     daily_return_cumulative = [0]
@@ -113,7 +116,7 @@ def Portfolio2():
 
     #calculate the option price for the first day
     holding_option_list = collections.deque(maxlen=1)
-    call,put = BS_model(sp_price_global[0],sp_price_global[0],90.0/252.0,libor_global[0]/100.0,vix_global[0]/100.0)
+    call,put = BS_model(stock_list[0],stock_list[0],90.0/252.0,libor_global[0]/100.0,vix_global[0]/100.0)
     if sixty_day_ave_price[0] > hundrend_twenty_ave_price[0]:
         holding_option_list.append(["Call",call,0])
     else:
@@ -125,20 +128,20 @@ def Portfolio2():
         daily_return = 0
         for j in range(len(holding_option_list)):
             holding_option_item = holding_option_list[j]
-            call, put = BS_model(sp_price_global[i], sp_price_global[holding_option_item[2]], (90.0 - (i - holding_option_item[2])) / 252.0, libor_global[i] / 100.0,
+            call, put = BS_model(stock_list[i], stock_list[holding_option_item[2]], (90.0 - (i - holding_option_item[2])) / 252.0, libor_global[i] / 100.0,
                                  vix_global[i] / 100.0)
             if holding_option_item[0] == "Call":
                 daily_return += call - holding_option_item[1]
                 holding_option_list[j][1] = call
+
             else:
                 daily_return += put - holding_option_item[1]
                 holding_option_list[j][1] = put
-
         total_return += daily_return
         daily_return_cumulative.append(total_return)
         daily_return_list.append(daily_return)
 
-        call, put = BS_model(sp_price_global[i], sp_price_global[i], 90.0 / 252.0, libor_global[i] / 100.0,
+        call, put = BS_model(stock_list[i], stock_list[i], 90.0 / 252.0, libor_global[i] / 100.0,
                              vix_global[i] / 100.0)
         if sixty_day_ave_price[i] > hundrend_twenty_ave_price[i]:
             holding_option_list.append(["Call", call, i])
@@ -223,12 +226,13 @@ def Var_function():
     print Cvar
 
 
+
+
 def random_path():
-    N = 10000
-    mu, sigma = ((1.5 * libor_global[0])/100.0 - ((vix_global[0]/100.0) ** 2) / 2) * 1 / 252, ((vix_global[0]/100.0) ** 2) * 1 / 252
+    N = 10
+    mu, sigma = ((1 * libor_global[0])/100.0 - ((vix_global[0]/100.0) ** 2) / 2) * 1 / 252, ((vix_global[0]/100.0) ** 2) * 1 / 252
     s = np.random.normal(mu, sigma, N)
     length = len(sp_price_global)
-    path_list = []
     path = [sp_price_global[0]]
     for i in range(N):
         for j in range(1,length):
@@ -237,13 +241,35 @@ def random_path():
             path.append(new_point)
         path = [sp_price_global[0]]
         path_list.append(path)
-    last_day_price = []
     for ls in path_list:
         # plt.plot(ls,linewidth=1)
         last_day_price.append(ls[-1])
-    plt.hist(last_day_price,200)
+    # plt.hist(last_day_price,200)
+    # plt.legend()
+    # plt.show()
+
+
+def Var(portfolio):
+    length = len(path_list)
+    last_day_cumulative_return = []
+    for i in range(length):
+        if (portfolio == "portfolio1"):
+            total_return, daily_return_list, daily_return_cumulative = Portfolio1(path_list[i])
+        elif (portfolio == "portfolio2"):
+            total_return, daily_return_list, daily_return_cumulative = Portfolio2(path_list[i])
+        elif (portfolio == "portfolio3"):
+            total_return, daily_return_list, daily_return_cumulative = Portfolio3(path_list[i])
+        last_day_cumulative_return.append(-1 * daily_return_cumulative[-1])
+        plt.plot(daily_return_cumulative,linewidth=1)
+    #plt.hist(last_day_cumulative_return,200)
+
     plt.legend()
     plt.show()
+
+
+
+
+
 
 
 
@@ -258,10 +284,14 @@ def main():
     # read file
     df = pd.read_csv("data_modified.csv")
     sp_price = df["SP500Price"]
+    sp_price = sp_price[~np.isnan(sp_price)]
+    print sp_price
     sp_price_global.extend(sp_price[119:])
     libor = df["LIBOR"]
+    libor = libor[~np.isnan(libor)]
     libor_global.extend(libor[119:])
     vix = df["VIX"]
+    vix = vix[~np.isnan(vix)]
     vix_global.extend(vix[119:])
 
 
@@ -301,22 +331,26 @@ def main():
     # plt.legend()
     # plt.show()
 
-    ##Portfolio 2
-    # r2 = Portfolio2()
+    #Portfolio 2
+    # r2 = Portfolio2(sp_price_global)
+    # print r2[2]
     # plt.plot(r2[2],linewidth=1,label='Cumulative Return')
     # plt.legend()
     # plt.show()
 
 
     ##Portfolio 3
-    # r3 = Portfolio3()
-    # plt.plot(r3[2],linewidth=1,label='Cumulative Return')
-    # plt.legend()
-    # plt.show()
+    r3 = Portfolio3()
+    plt.plot(r3[2],linewidth=1,label='Cumulative Return')
+    plt.legend()
+    plt.show()
 
     ##Var
     # Var_function()
     random_path()
+
+    # Var("portfolio1")
+    #Var("portfolio2")
 
 if __name__ == '__main__':
     main()
